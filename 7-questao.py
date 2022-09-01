e4763581a8f7b30aa4633c24108fa50ea0f14b1d
Gustavo Costa <gugucost.gc@gmail.com>
2022-09-01 13:57:29 -0300

    Delete 7-questao.py

--- 7-questao.py
+++
@@ -1,6 +0,0 @@
-x = float(input("Digite a altura do quadrado: "))
-y = float(input("Digite a largura do quadrado: "))
-xy = (x*y)
-print("A área do quadrado é:",xy)
-print("O dobro da área do quadrado é:",xy*2)
-
